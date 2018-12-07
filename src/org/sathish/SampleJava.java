c384f2ff28c78d9899f798974ddb0db8c8dca0dd
Muthugsm <sellamuthu95@gmail.com>
2018-12-07 12:35:47 +0530

    Changes made

--- src/org/sathish/SampleJava.java
+++ src/org/sathish/SampleJava.java
@@ -3,6 +3,7 @@ package org.sathish;
 public class SampleJava {
 	public static void main(String[] args) {
 		System.out.println("welcome to GreensTechnology");
+		System.out.println("Test case1");
 	}
 
 }
